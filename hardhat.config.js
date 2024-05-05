9715df3ef73f3a1581caa26b3ca70f99a7e9cb57
Tapesh Sharma <114461563+tapesh1134@users.noreply.github.com>
2024-05-05 21:45:31 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/IzOCKFmZ9gZ8vYan95h-3QB9y2RtBaBm",
+      accounts: [ "f8fef4100e443ec112509ea5c3ae869ffbaf280d2851c36e1bccec06a1982a75" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
